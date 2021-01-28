8183fa290b250ee8aeb45977510776ae4012b76e
TaraYu <46502584+TaraYu@users.noreply.github.com>
2021-01-28 08:09:27 +0800

    complete
    first commit

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 #  coding: utf-8 
-import socketserver
+import socketserver,os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -32,7 +32,75 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        #Receive data from the client, 1024 bytes each time
+
+
+        request_data = self.data.decode('utf-8')
+        request_array = request_data.split('\r\n')[0]
+        print(request_data)
+        request_method = request_data.split()[0]
+        request_URI = request_data.split()[1]
+ 
+
+        if request_method != 'GET':
+            self.handle405status()
+            return
+        #check the ending and make sure there is not end with a file name in this situation
+        elif request_URI[-1] != '/' and '.' not in request_URI.split('/')[-1]:
+            self.handle301status(request_URI)
+            return
+        else:
+            #surport css
+            if 'css' in request_URI:
+                MIMA_type = 'text/css'
+            #surport html
+            elif 'html' in request_URI:
+                MIMA_type = 'text/html'
+            #end with '/' and there is no filaname, correct it.
+            #eg: http://127.0.0.1:8080/deep/ -->http://127.0.0.1:8080/deep/index.html
+            elif request_URI[-1] == '/':
+                request_URI += 'index.html'
+                MIMA_type = 'text/html'
+            else:
+                self.handle404status()
+                return
+
+
+            filepath = 'www' + request_URI
+            print(filepath)
+            #check whether file exist:
+            #https://stackoverflow.com/questions/82831/how-do-i-check-whether-a-file-exists-without-exceptions
+            if os.path.exists(filepath):
+                f = open(filepath, 'r')
+                thedata = f.read()
+                print(thedata)
+                f.close()
+                self.sendResponse(MIMA_type, thedata)
+                return
+            else:
+                self.handle404status()
+                return
+    
+    
+    #deal with string with encode():
+    #https://stackoverflow.com/questions/9942594/unicodeencodeerror-ascii-codec-cant-encode-character-u-xa0-in-position-20
+
+    def handle404status(self):
+        response = "HTTP/1.1 404 Not Found\r\n404 Not Found"
+        self.request.sendall(response.encode())
+
+    def handle405status(self):
+        response = "HTTP/1.1 405 Method Not Allowed\r\n405 Method Not Allowed"
+        self.request.sendall(response.encode())
+
+    def handle301status(self, request_URI):
+        response = "HTTP/1.1 301 Moved Permanently\r\n"+ "Location:"+ request_URI+"/\r\n"
+        self.request.sendall(response.encode())
+
+    def sendResponse(self, MIMA_type, thedata):
+        response = 'HTTP/1.1 200 OK\r\n'+ 'Content-type: '+ MIMA_type +'\r\n' + thedata
+        self.request.sendall(response.encode())
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
